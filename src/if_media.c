09f67cd5dea4b778fdbbbed06513c975e0529b11
Matthias Schmitz <matthias@sigxcpu.org>
2021-09-13 23:04:59 +0200

    Remove FDDI media detection
    FreeBSD 12 removed support for FDDI (and Token Ring). As FDDI and
    Token Ring are extinct nowadays we don't nee to support this.
    
    Signed-off-by: Matthias Schmitz <matthias@sigxcpu.org>

--- src/if_media.c
+++ src/if_media.c
@@ -159,21 +159,6 @@ int get_if_speed(char *ifstring)
             break;
         }                       /* end switch physical */
         break;
-        /* FDDI interfaces */
-        /* fpa doesn't seem to support SIOCGIFMEDIA on FreeBSD
-         * so we won't get here but anyway ...
-         */
-    case IFM_FDDI:
-        switch (physical) {
-        case IFM_FDDI_SMF:
-        case IFM_FDDI_MMF:
-        case IFM_FDDI_UTP:
-            speed = 100 * 1000;
-            break;
-        default:
-            speed = ERR_IFACE_NO_SPEED;
-        }
-        break;
 #if WIRELESS
         /* IEEE 802.11 wireless interfaces */
     case IFM_IEEE80211:
